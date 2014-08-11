92a88ae43a502026f76257b7395d8df7ff714cfa
David Ray <cognitionmission@gmail.com>
2014-08-10 19:30:10 -0500

    Added java docs and doc generator script

---
+++ jdoc.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+javadoc -private -sourcepath src:test -use -version -author -d doc org.numenta.nupic.data org.numenta.nupic.model org.numenta.nupic.research org.numenta.nupic.integration org.numenta.nupic.unit
+
+
