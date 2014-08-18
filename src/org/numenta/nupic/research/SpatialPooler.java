64cad66debc871c0420020aec3b22e888a60d5b3
David Ray <cognitionmission@gmail.com>
2014-08-18 17:25:33 -0500

    test commit after repository transfer

--- src/org/numenta/nupic/research/SpatialPooler.java
+++ src/org/numenta/nupic/research/SpatialPooler.java
@@ -556,6 +556,28 @@ public class SpatialPooler {
 	}
 	
 	/**
+	 * Maps a column to its respective input index, keeping to the topology of
+     * the region. It takes the index of the column as an argument and determines
+     * what is the index of the flattened input vector that is to be the center of
+     * the column's potential pool. It distributes the columns over the inputs
+     * uniformly. The return value is an integer representing the index of the
+     * input bit. Examples of the expected output of this method:
+     * * If the topology is one dimensional, and the column index is 0, this
+     *   method will return the input index 0. If the column index is 1, and there
+     *   are 3 columns over 7 inputs, this method will return the input index 3.
+     * * If the topology is two dimensional, with column dimensions [3, 5] and
+     *   input dimensions [7, 11], and the column index is 3, the method
+     *   returns input index 8.
+     *   
+	 * @param columnIndex	The index identifying a column in the permanence, potential
+     *              		and connectivity matrices.
+	 * @return	the input index
+	 */
+	public int mapColumn(int columnIndex) {
+		return -1;
+	}
+	
+	/**
 	 * Maps a column to its input bits. This method encapsulates the topology of
      * the region. It takes the index of the column as an argument and determines
      * what are the indices of the input vector that are located within the
