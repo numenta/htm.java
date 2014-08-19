53ff5fbb84fe4b043a1b9643483c84799323d3ed
David Ray <cognitionmission@gmail.com>
2014-08-19 00:10:09 -0500

    echo out current dir

--- run_build.sh
+++ run_build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo `pwd`
 cd src
 javac -d ../build -cp ../resources/btc-ascii-table-1.0.jar:../resources/trove-3.1a1.jar org/numenta/nupic/data/* org/numenta/nupic/model/* org/numenta/nupic/research/*
 
