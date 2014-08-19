fa00a3e832ea81f800bf9b4b69fc78b276c49dc6
David Ray <cognitionmission@gmail.com>
2014-08-19 00:41:09 -0500

    edit 2

--- run_build.sh
+++ run_build.sh
@@ -2,5 +2,6 @@
 
 echo `pwd`
 cd src
+echo `pwd`
 javac -g -d ../build -cp ../resources/btc-ascii-table-1.0.jar:../resources/trove-3.1a1.jar org/numenta/nupic/data/* org/numenta/nupic/model/* org/numenta/nupic/research/*
 
