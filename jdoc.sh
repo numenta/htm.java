84af2e109e64510b25177363fa272d0b8de23947
David Ray <cognitionmission@gmail.com>
2014-10-18 06:17:13 -0500

    Regenerate Java docs and add link to readme and wiki

--- jdoc.sh
+++ jdoc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-javadoc -private -sourcepath src:test -use -version -author -d doc org.numenta.nupic.data org.numenta.nupic.model org.numenta.nupic.research org.numenta.nupic.integration org.numenta.nupic.unit
+javadoc -private -sourcepath src/main/java:src/test/java -use -version -author -d doc org.numenta.nupic.data org.numenta.nupic.model org.numenta.nupic.research org.numenta.nupic.integration org.numenta.nupic.unit
 
 
