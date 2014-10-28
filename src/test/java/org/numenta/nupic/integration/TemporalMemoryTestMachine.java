efae9648774c1fe0ab317c83a2d5cc78171b2e50
solovyevk <solovyevk@gmail.com>
2014-10-27 23:14:26 -0400

    Revert to explicit import statements in org.numenta.nupic.integration.TemporalMemoryTestMachine.java

--- src/test/java/org/numenta/nupic/integration/TemporalMemoryTestMachine.java
+++ src/test/java/org/numenta/nupic/integration/TemporalMemoryTestMachine.java
@@ -32,8 +32,15 @@ import org.numenta.nupic.research.TemporalMemory;
 import org.numenta.nupic.util.PatternMachine;
 import org.numenta.nupic.util.SequenceMachine;
 
-import java.util.*;
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 /**
  * Test utility to furnish test data.
  * 
