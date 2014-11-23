2d28ae9ec202b30c4c1f08af20021d3b2f2febb0
solovyevk <solovyevk@gmail.com>
2014-11-23 12:05:55 -0500

    Updated SparseBinaryMatrix to support N-dimensional topology (and other improvements)

--- src/test/java/org/numenta/nupic/util/SparseBinaryMatrixTest.java
+++ src/test/java/org/numenta/nupic/util/SparseBinaryMatrixTest.java
@@ -33,206 +33,186 @@ import static org.junit.Assert.fail;
 
 public class SparseBinaryMatrixTest {
 
-	@Test
-	public void testBackingStoreAndSliceAccess() {
-		int[] dimensions = new int[] { 5, 10 };
-		int[][] connectedSynapses = new int[][] {
-			{1, 0, 0, 0, 0, 1, 0, 0, 0, 0},
-		    {0, 1, 0, 0, 0, 0, 1, 0, 0, 0},
-		    {0, 0, 1, 0, 0, 0, 0, 1, 0, 0},
-		    {0, 0, 0, 1, 0, 0, 0, 0, 1, 0},
-		    {0, 0, 0, 0, 1, 0, 0, 0, 0, 1}};
-		SparseBinaryMatrix sm = new SparseBinaryMatrix(dimensions);
-		for(int i = 0;i < sm.getDimensions()[0];i++) {
-			for(int j = 0;j < sm.getDimensions()[1];j++) {
-				sm.set(connectedSynapses[i][j], i, j);
-			}
-		}
-		
-		for(int i = 0;i < 5;i++) {
-			for(int j = 0;j < 10;j++) {
-				assertEquals(connectedSynapses[i][j], sm.getIntValue(i, j));
-			}
-		}
-		
-		for(int i = 0;i < connectedSynapses.length;i++) {
-			for(int j = 0;j < connectedSynapses[i].length;j++) {
-				assertEquals(connectedSynapses[i][j], ((int[])sm.getSlice(i))[j], 0);
-			}
-		}
-		
-		//Make sure warning is proper for exact access
-		try {
-			sm.getSlice(0, 4);
-			fail();
-		}catch(Exception e) {
-			assertEquals("This method only returns the array holding the specified index: " + 
-				Arrays.toString(new int[] { 0, 4 }), e.getMessage());
-		}
-	}
-	
-	@Test
-	public void testRightVecSumAtNZFast() {
-		int[] dimensions = new int[] { 5, 10 };
-		int[][] connectedSynapses = new int[][] {
-			{1, 0, 0, 0, 0, 1, 0, 0, 0, 0},
-		    {0, 1, 0, 0, 0, 0, 1, 0, 0, 0},
-		    {0, 0, 1, 0, 0, 0, 0, 1, 0, 0},
-		    {0, 0, 0, 1, 0, 0, 0, 0, 1, 0},
-		    {0, 0, 0, 0, 1, 0, 0, 0, 0, 1}};
-		SparseBinaryMatrix sm = new SparseBinaryMatrix(dimensions);
-		for(int i = 0;i < sm.getDimensions()[0];i++) {
-			for(int j = 0;j < sm.getDimensions()[1];j++) {
-				sm.set(connectedSynapses[i][j], i, j);
-			}
-		}
-		
-		for(int i = 0;i < 5;i++) {
-			for(int j = 0;j < 10;j++) {
-				assertEquals(connectedSynapses[i][j], sm.getIntValue(i, j));
-			}
-		}
-		
-		int[] inputVector = new int[] { 1, 0, 1, 0, 1, 0, 1, 0, 1, 0 };
-		int[] results = new int[5];
-		int[] trueResults = new int[] { 1, 1, 1, 1, 1 };
-		sm.rightVecSumAtNZ(inputVector, results);
-		
-		for(int i = 0;i < results.length;i++) {
-			assertEquals(trueResults[i], results[i]);
-		}
-		
-		///////////////////////
-		
-		connectedSynapses = new int[][] {
-			{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-		    {0, 0, 1, 1, 1, 1, 1, 1, 1, 1},
-		    {0, 0, 0, 0, 1, 1, 1, 1, 1, 1},
-		    {0, 0, 0, 0, 0, 0, 1, 1, 1, 1},
-		    {0, 0, 0, 0, 0, 0, 0, 0, 1, 1}};
-		sm = new SparseBinaryMatrix(dimensions);
-		for(int i = 0;i < sm.getDimensions()[0];i++) {
-			for(int j = 0;j < sm.getDimensions()[1];j++) {
-				sm.set(connectedSynapses[i][j], i, j);
-			}
-		}
-		
-		for(int i = 0;i < 5;i++) {
-			for(int j = 0;j < 10;j++) {
-				assertEquals(connectedSynapses[i][j], sm.getIntValue(i, j));
-			}
-		}
-		
-		inputVector = new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
-		results = new int[5];
-		trueResults = new int[] { 10, 8, 6, 4, 2 };
-		sm.rightVecSumAtNZ(inputVector, results);
-		
-		for(int i = 0;i < results.length;i++) {
-			assertEquals(trueResults[i], results[i]);
-		}
-	}
-	
-	@Test
-	public void testSetTrueCount() {
-		int[] dimensions = new int[] { 5, 10 };
-		int[][] connectedSynapses = new int[][] {
-			{1, 0, 0, 0, 0, 1, 0, 0, 0, 0},
-		    {0, 1, 0, 0, 0, 0, 1, 0, 0, 0},
-		    {0, 0, 1, 0, 0, 0, 0, 1, 0, 0},
-		    {0, 0, 0, 1, 0, 0, 0, 0, 1, 0},
-		    {0, 0, 0, 0, 1, 0, 0, 0, 0, 1}};
-		SparseBinaryMatrix sm = new SparseBinaryMatrix(dimensions);
-		for(int i = 0;i < sm.getDimensions()[0];i++) {
-			for(int j = 0;j < sm.getDimensions()[1];j++) {
-				sm.set(connectedSynapses[i][j], i, j);
-			}
-		}
-		
-		for(int i = 0;i < 5;i++) {
-			for(int j = 0;j < 10;j++) {
-				assertEquals(connectedSynapses[i][j], sm.getIntValue(i, j));
-			}
-		}
-		
-		for(int i = 0;i < 5;i++) {
-			assertEquals(2, sm.getTrueCount(i));
-		}
-	}
-
-    private Object createMultiDimensionArray(int[] sizes){
-         // Randomly choose number of dimensions (1, 2, 3 or 4) at runtime.
-        /*Random r = new Random();
-        int dims = 1 + r.nextInt(4);
-        // Randomly choose array lengths (1, 2, 3, 4 or 5) at runtime.
-        int[] sizes = new int[dims];
-        for (int i = 0; i < sizes.length; i++)
-            sizes[i] = 1 + r.nextInt(5);
-        // Create array */
-        System.out.println("Creating array with dimensions / sizes: " +
-                Arrays.toString(sizes).replaceAll(", ", "]["));
-        return Array.newInstance(int.class, sizes);
-    }
-
-    public static int[] tail(int[] arr) {
-            return Arrays.copyOfRange(arr, 1, arr.length);
+    @Test
+    public void testBackingStoreAndSliceAccess() {
+        int[] dimensions = new int[]{5, 10};
+        int[][] connectedSynapses = new int[][]{
+                {1, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+                {0, 1, 0, 0, 0, 0, 1, 0, 0, 0},
+                {0, 0, 1, 0, 0, 0, 0, 1, 0, 0},
+                {0, 0, 0, 1, 0, 0, 0, 0, 1, 0},
+                {0, 0, 0, 0, 1, 0, 0, 0, 0, 1}};
+        SparseBinaryMatrix sm = new SparseBinaryMatrix(dimensions);
+        for (int i = 0; i < sm.getDimensions()[0]; i++) {
+            for (int j = 0; j < sm.getDimensions()[1]; j++) {
+                sm.set(connectedSynapses[i][j], i, j);
+            }
         }
 
-    public static void setValue(Object array, int value, int... indexes) {
-        if (indexes.length == 1)
-            ((int[]) array)[indexes[0]] = value;
-        else
-            setValue(Array.get(array, indexes[0]), value, tail(indexes));
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 10; j++) {
+                assertEquals(connectedSynapses[i][j], sm.getIntValue(i, j));
+            }
+        }
+
+        for (int i = 0; i < connectedSynapses.length; i++) {
+            for (int j = 0; j < connectedSynapses[i].length; j++) {
+                assertEquals(connectedSynapses[i][j], ((int[])sm.getSlice(i))[j], 0);
+            }
+        }
+
+        //Make sure warning is proper for exact access
+        try {
+            sm.getSlice(0, 4);
+            fail();
+        } catch (Exception e) {
+            assertEquals("This method only returns the array holding the specified index: " +
+                         Arrays.toString(new int[]{0, 4}), e.getMessage());
+        }
     }
+
+    @Test
+    public void testRightVecSumAtNZFast() {
+        int[] dimensions = new int[]{5, 10};
+        int[][] connectedSynapses = new int[][]{
+                {1, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+                {0, 1, 0, 0, 0, 0, 1, 0, 0, 0},
+                {0, 0, 1, 0, 0, 0, 0, 1, 0, 0},
+                {0, 0, 0, 1, 0, 0, 0, 0, 1, 0},
+                {0, 0, 0, 0, 1, 0, 0, 0, 0, 1}};
+        SparseBinaryMatrix sm = new SparseBinaryMatrix(dimensions);
+        for (int i = 0; i < sm.getDimensions()[0]; i++) {
+            for (int j = 0; j < sm.getDimensions()[1]; j++) {
+                sm.set(connectedSynapses[i][j], i, j);
+            }
+        }
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 10; j++) {
+                assertEquals(connectedSynapses[i][j], sm.getIntValue(i, j));
+            }
+        }
+
+        int[] inputVector = new int[]{1, 0, 1, 0, 1, 0, 1, 0, 1, 0};
+        int[] results = new int[5];
+        int[] trueResults = new int[]{1, 1, 1, 1, 1};
+        sm.rightVecSumAtNZ(inputVector, results);
+
+        for (int i = 0; i < results.length; i++) {
+            assertEquals(trueResults[i], results[i]);
+        }
+
+        ///////////////////////
+
+        connectedSynapses = new int[][]{
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {0, 0, 1, 1, 1, 1, 1, 1, 1, 1},
+                {0, 0, 0, 0, 1, 1, 1, 1, 1, 1},
+                {0, 0, 0, 0, 0, 0, 1, 1, 1, 1},
+                {0, 0, 0, 0, 0, 0, 0, 0, 1, 1}};
+        sm = new SparseBinaryMatrix(dimensions);
+        for (int i = 0; i < sm.getDimensions()[0]; i++) {
+            for (int j = 0; j < sm.getDimensions()[1]; j++) {
+                sm.set(connectedSynapses[i][j], i, j);
+            }
+        }
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 10; j++) {
+                assertEquals(connectedSynapses[i][j], sm.getIntValue(i, j));
+            }
+        }
+
+        inputVector = new int[]{1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+        results = new int[5];
+        trueResults = new int[]{10, 8, 6, 4, 2};
+        sm.rightVecSumAtNZ(inputVector, results);
+
+        for (int i = 0; i < results.length; i++) {
+            assertEquals(trueResults[i], results[i]);
+        }
+    }
+
+    @Test
+    public void testSetTrueCount() {
+        int[] dimensions = new int[]{5, 10};
+        int[][] connectedSynapses = new int[][]{
+                {1, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+                {0, 1, 0, 0, 0, 0, 1, 0, 0, 0},
+                {0, 0, 1, 0, 0, 0, 0, 1, 0, 0},
+                {0, 0, 0, 1, 0, 0, 0, 0, 1, 0},
+                {0, 0, 0, 0, 1, 0, 0, 0, 0, 1}};
+        SparseBinaryMatrix sm = new SparseBinaryMatrix(dimensions);
+        for (int i = 0; i < sm.getDimensions()[0]; i++) {
+            for (int j = 0; j < sm.getDimensions()[1]; j++) {
+                sm.set(connectedSynapses[i][j], i, j);
+            }
+        }
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 10; j++) {
+                assertEquals(connectedSynapses[i][j], sm.getIntValue(i, j));
+            }
+        }
+
+        for (int i = 0; i < 5; i++) {
+            assertEquals(2, sm.getTrueCount(i));
+        }
+    }
+
 
     public static void fillWithSomeRandomValues(Object array, Random r, int... sizes) {
         for (int i = 0; i < sizes[0]; i++)
-            if (sizes.length == 1)
-                ((int[]) array)[i] =  r.nextInt(2);
-            else
-                fillWithSomeRandomValues(Array.get(array, i), r, tail(sizes));
+            if (sizes.length == 1) {
+                ((int[])array)[i] = r.nextInt(2);
+            } else {
+                fillWithSomeRandomValues(Array.get(array, i), r, ArrayUtils.tail(sizes));
+            }
     }
-
-    private void printArray(Object array){
-        if(array instanceof Object[]){
-            System.out.println(Arrays.deepToString((Object[]) array));
-        } else {
-            //One dimension
-            System.out.println(Arrays.toString((int[])array));
-        }
-    }
-
 
 
     @Test
     public void testBackingStoreAndSliceAccessManyDimensions() {
-          /*Create huge 5 dimensional matrix*/
-        int[] dimensions = {5, 5 ,5};
-        Object multiDimArray = createMultiDimensionArray(dimensions);
-        //fillWithSomeRandomValues(multiDimArray,new Random(), dimensions);
-         /*set diagonal element to true*/
-        setValue(multiDimArray, 1, 0, 0, 0);
-        setValue(multiDimArray, 1, 1, 1, 1);
-        setValue(multiDimArray, 1, 2, 2, 2);
-        setValue(multiDimArray, 1, 3, 3, 3);
-        setValue(multiDimArray, 1, 4, 4, 4);
-        printArray(multiDimArray);
+          /*Create 3 dimensional matrix*/
+        int[] dimensions = {5, 5, 5};
         SparseBinaryMatrix sm = new SparseBinaryMatrix(dimensions);
+         /*set diagonal element to true*/
         sm.set(1, 0, 0, 0);
         sm.set(1, 1, 1, 1);
         sm.set(1, 2, 2, 2);
         sm.set(1, 3, 3, 3);
         sm.set(1, 4, 4, 4);
-        assertEquals(1, sm.getIntValue(0, 0, 0));
-        assertEquals(1, sm.getIntValue(0, 0, 0));
-        assertEquals(1, sm.getIntValue(0, 0, 0));
-        assertEquals(1, sm.getIntValue(0, 0, 0));
-        assertEquals(1, sm.getIntValue(0, 0, 0));
-        //setValue(multiDimArray, 7 ,1,1,1);
-        //System.out.println(Arrays.deepToString((Object[]) multiDimArray));
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                for (int k = 0; k < 5; k++) {
+                    if (k == j & j == i) {
+                        assertEquals(1, sm.getIntValue(i, j, k));
+                    }
+                }
+            }
+        }
+        int[] slice = (int[])sm.getSlice(4, 4);
+        for (int i = 0; i < 5; i++) {
+            assertEquals(1, sm.getTrueCount(i));
+        }
+        System.out.println("slice:" + ArrayUtils.intArrayToString(slice));
+        assertEquals(1, slice[4]);
+        /*update first row to true, other to false*/
+        for (int i = 0; i < 5; i++) {
+                   for (int j = 0; j < 5; j++) {
+                       for (int k = 0; k < 5; k++) {
+                           if (0 == i) {
+                               sm.set(1, i, j, k);
+                           } else {
+                               sm.set(0, i, j, k);
+                           }
+                       }
+                   }
+               }
+        assertEquals(25, sm.getTrueCounts()[0]);
+        assertEquals(0, sm.getTrueCounts()[1]);
 
     }
 
 
-	
 }
