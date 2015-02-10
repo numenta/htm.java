7cc3311a1ed1ff3c3b92d76ce223fdf6abc1886b
David Ray <cognitionmission@gmail.com>
2015-02-09 18:32:30 -0600

    spaces instead of tabs (where was this hiding?)

--- src/jmh/java/org/numenta/nupic/benchmarks/SpatialPoolerGlobalInhibitionBenchmark.java
+++ src/jmh/java/org/numenta/nupic/benchmarks/SpatialPoolerGlobalInhibitionBenchmark.java
@@ -13,34 +13,34 @@ import org.openjdk.jmh.infra.Blackhole;
 
 public class SpatialPoolerGlobalInhibitionBenchmark extends AbstractAlgorithmBenchmark {
 
-	private int[][] input;
+    private int[][] input;
 
-	@Setup
-	public void init() {
-		super.init();
+    @Setup
+    public void init() {
+        super.init();
 
-		input = new int[7][8];
-		for(int i = 0;i < 7;i++) {
-			input[i] = encoder.encode((double) i + 1);
-		}
-	}
+        input = new int[7][8];
+        for(int i = 0;i < 7;i++) {
+            input[i] = encoder.encode((double) i + 1);
+        }
+    }
 
-	@Override
-	protected Parameters getParameters() {
-		Parameters parameters = super.getParameters();
-		parameters.setParameterByKey(KEY.GLOBAL_INHIBITIONS, true);
-		return parameters;
-	}
+    @Override
+    protected Parameters getParameters() {
+        Parameters parameters = super.getParameters();
+        parameters.setParameterByKey(KEY.GLOBAL_INHIBITIONS, true);
+        return parameters;
+    }
 
-	@Benchmark
-	@BenchmarkMode(Mode.AverageTime)
-	@OutputTimeUnit(TimeUnit.MILLISECONDS)
-	public int[] measureAvgCompute_7_Times(Blackhole bh) throws InterruptedException {
-		for(int i = 0;i < 7;i++) {
-			pooler.compute(memory, input[i], SDR, true, false);
-		}
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    public int[] measureAvgCompute_7_Times(Blackhole bh) throws InterruptedException {
+        for(int i = 0;i < 7;i++) {
+            pooler.compute(memory, input[i], SDR, true, false);
+        }
 
-		return SDR;
-	}
+        return SDR;
+    }
 
 }
--- src/jmh/java/org/numenta/nupic/benchmarks/SpatialPoolerLocalInhibitionBenchmark.java
+++ src/jmh/java/org/numenta/nupic/benchmarks/SpatialPoolerLocalInhibitionBenchmark.java
@@ -11,27 +11,27 @@ import org.openjdk.jmh.infra.Blackhole;
 
 public class SpatialPoolerLocalInhibitionBenchmark extends AbstractAlgorithmBenchmark {
 
-	private int[][] input;
+    private int[][] input;
 
-	@Setup
-	public void init() {
-		super.init();
+    @Setup
+    public void init() {
+        super.init();
 
-		input = new int[7][8];
-		for(int i = 0;i < 7;i++) {
-			input[i] = encoder.encode((double) i + 1);
-		}
-	}
+        input = new int[7][8];
+        for(int i = 0;i < 7;i++) {
+            input[i] = encoder.encode((double) i + 1);
+        }
+    }
 
-	@Benchmark
-	@BenchmarkMode(Mode.AverageTime)
-	@OutputTimeUnit(TimeUnit.MILLISECONDS)
-	public int[] measureAvgCompute_7_Times(Blackhole bh) throws InterruptedException {
-		for(int i = 0;i < 7;i++) {
-			pooler.compute(memory, input[i], SDR, true, false);
-		}
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    public int[] measureAvgCompute_7_Times(Blackhole bh) throws InterruptedException {
+        for(int i = 0;i < 7;i++) {
+            pooler.compute(memory, input[i], SDR, true, false);
+        }
 
-		return SDR;
-	}
+        return SDR;
+    }
 
 }
