0a5d480bdc4304dbe1cb040dc50daba68929bcd6
antidata <antidata.05@gmail.com>
2015-03-26 22:54:09 -0600

    #207 Code Reformat

--- src/main/java/org/numenta/nupic/encoders/SparsePassThroughEncoder.java
+++ src/main/java/org/numenta/nupic/encoders/SparsePassThroughEncoder.java
@@ -23,6 +23,7 @@ package org.numenta.nupic.encoders;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import java.util.Arrays;
 
 /**
@@ -35,73 +36,72 @@ import java.util.Arrays;
  * or for n=8 w=1 "0,2,5" => 101001000
  * i.e. for n=24 w=3 [0,2,5] => 111000111000000111000000000
  * or for n=24 w=3 "0,2,5" => 111000111000000111000000000
- * @author wilsondy (from Python original)
  *
+ * @author wilsondy (from Python original)
  */
 public class SparsePassThroughEncoder extends PassThroughEncoder {
-	private SparsePassThroughEncoder() { super(); }
+    private SparsePassThroughEncoder() { super(); }
+
     private static final Logger LOGGER = LoggerFactory.getLogger(SparsePassThroughEncoder.class);
 
-	public SparsePassThroughEncoder(int outputWidth, Integer outputBitsOnCount) {
-		super(outputWidth, outputBitsOnCount);
+    public SparsePassThroughEncoder(int outputWidth, Integer outputBitsOnCount) {
+        super(outputWidth, outputBitsOnCount);
         LOGGER.info("Building new SparsePassThroughEncoder instance, outputWidth: {} outputBitsOnCount: {}", outputWidth);
-	}
+    }
 
-	/**
-	 * Returns a builder for building SparsePassThroughEncoders.
-	 * This builder may be reused to produce multiple builders
-	 *
-	 * @return a {@code SparsePassThroughEncoder.Builder}
-	 */
-	public static Encoder.Builder<SparsePassThroughEncoder.Builder, SparsePassThroughEncoder> sparseBuilder() {
-		return new SparsePassThroughEncoder.Builder();
-	}
+    /**
+     * Returns a builder for building SparsePassThroughEncoders.
+     * This builder may be reused to produce multiple builders
+     *
+     * @return a {@code SparsePassThroughEncoder.Builder}
+     */
+    public static Encoder.Builder<SparsePassThroughEncoder.Builder, SparsePassThroughEncoder> sparseBuilder() {
+        return new SparsePassThroughEncoder.Builder();
+    }
 
-	@Override
-	/**
-	 * Convert the array of indices to a bit array and then pass to parent.
-	 */
-	public void encodeIntoArray(int[] input, int[] output){
+    @Override
+    /**
+     * Convert the array of indices to a bit array and then pass to parent.
+     */
+    public void encodeIntoArray(int[] input, int[] output) {
 
-		int[] denseInput = new int[output.length];
-		for (int i : input) {
-			if(i > denseInput.length)
-				throw new IllegalArgumentException(String.format("Output bit count set too low, need at least %d bits", i));
-			denseInput[i] = 1;
-		}
-		super.encodeIntoArray(denseInput, output);
-
+        int[] denseInput = new int[output.length];
+        for (int i : input) {
+            if (i > denseInput.length)
+                throw new IllegalArgumentException(String.format("Output bit count set too low, need at least %d bits", i));
+            denseInput[i] = 1;
+        }
+        super.encodeIntoArray(denseInput, output);
         LOGGER.trace("Input: {} \nOutput: {} \n", Arrays.toString(input), Arrays.toString(output));
-	}
+    }
 
-	/**
-	 * Returns a {@link Encoder.Builder} for constructing {@link SparsePassThroughEncoder}s
-	 *
-	 * The base class architecture is put together in such a way where boilerplate
-	 * initialization can be kept to a minimum for implementing subclasses, while avoiding
-	 * the mistake-proneness of extremely long argument lists.
-	 *
-	 */
-	public static class Builder extends Encoder.Builder<SparsePassThroughEncoder.Builder, SparsePassThroughEncoder> {
-		private Builder() {}
+    /**
+     * Returns a {@link Encoder.Builder} for constructing {@link SparsePassThroughEncoder}s
+     * <p/>
+     * The base class architecture is put together in such a way where boilerplate
+     * initialization can be kept to a minimum for implementing subclasses, while avoiding
+     * the mistake-proneness of extremely long argument lists.
+     */
+    public static class Builder extends Encoder.Builder<SparsePassThroughEncoder.Builder, SparsePassThroughEncoder> {
+        private Builder() {}
 
-		@Override
-		public SparsePassThroughEncoder build() {
-			//Must be instantiated so that super class can initialize
-			//boilerplate variables.
-			encoder = new SparsePassThroughEncoder();
+        @Override
+        public SparsePassThroughEncoder build() {
+            //Must be instantiated so that super class can initialize
+            //boilerplate variables.
+            encoder = new SparsePassThroughEncoder();
 
-			//Call super class here
-			super.build();
+            //Call super class here
+            super.build();
 
-			////////////////////////////////////////////////////////
-			//  Implementing classes would do setting of specific //
-			//  vars here together with any sanity checking       //
-			////////////////////////////////////////////////////////
+            ////////////////////////////////////////////////////////
+            //  Implementing classes would do setting of specific //
+            //  vars here together with any sanity checking       //
+            ////////////////////////////////////////////////////////
 
-			((SparsePassThroughEncoder)encoder).init();
+            ((SparsePassThroughEncoder) encoder).init();
 
-			return (SparsePassThroughEncoder)encoder;
-		}
-	}
+            return (SparsePassThroughEncoder) encoder;
+        }
+    }
 }
