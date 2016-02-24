32587c45032f231c6ef50d708169583c8bbc7b9d
Alexandre Vivmond <vivalex92@gmail.com>
2016-02-24 19:21:25 +0100

    MetricsTraceTest.java failes due to JVM using local decimal punctuation
    Added Locale.US to ensure that the tests were executed using dot decimal punctuation instead of whatever the local JVM decided to use.

--- src/main/java/org/numenta/nupic/monitor/mixin/MetricsTrace.java
+++ src/main/java/org/numenta/nupic/monitor/mixin/MetricsTrace.java
@@ -34,7 +34,7 @@ public class MetricsTrace extends Trace<Double> {
     }
 
     public String prettyPrintDatum(Metric datum) {
-        return String.format("min: %.2f, max: %.2f, sum: %.2f, mean: %.2f, std dev: %.2f", 
+        return String.format(Locale.US, "min: %.2f, max: %.2f, sum: %.2f, mean: %.2f, std dev: %.2f", 
             datum.min, datum.max, datum.sum, datum.mean, datum.standardDeviation);
     }
 }
