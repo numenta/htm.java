7078102f35ef6920389b70ff937c57a8ef5fe5ab
cogmission <cogitionmission@gmail.com>
2016-09-21 21:57:25 -0500

    Added Shuffle() method to UniversalRandom and compatibility test to RDSE

--- src/main/java/org/numenta/nupic/util/UniversalRandom.java
+++ src/main/java/org/numenta/nupic/util/UniversalRandom.java
@@ -132,6 +132,7 @@ public class UniversalRandom extends Random {
                 array[index] ^= array[i];
             }
         }
+        System.out.println("shuffle: " + Arrays.toString(array));
         return array;
     }
     
